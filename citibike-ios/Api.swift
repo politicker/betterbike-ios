da7a96e2902fe23ad30b7d589838fedac1d98327
Harrison Borges <hborges005@gmail.com>
2022-08-28 13:13:05 -0400

    add splash screen

--- citibike-ios/Api.swift
+++ citibike-ios/Api.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 
+let defaultLatitude = ""
+let defaultLongitude = ""
+
 struct API {
-	func fetchStations(completion: @escaping (Home) -> ()) {
+	func fetchStations(lat: String = defaultLatitude, lon: String = defaultLongitude, completion: @escaping (Home) -> ()) {
 		guard let url = URL(string: "http://localhost:8081") else { return }
-		
+
 		URLSession.shared.dataTask(with: url) { (data, _, _) in
 			let response = try! JSONDecoder().decode(Home.self, from: data!)
 			print(response)
--- citibike-ios/ContentView.swift
+++ citibike-ios/ContentView.swift
@@ -16,36 +16,49 @@ struct ContentView: View {
 	@State var latitude: CLLocationDegrees = 0
 	@State var longitude: CLLocationDegrees = 0
 	
+	@State var showSplash = true
+	@State var splashOpacity: Double = 1
+	
 	@StateObject var locationManager = LocationManager()
 	
 	var body: some View {
-		VStack(alignment: .leading) {
-			HStack {
-				Spacer()
-				Text("Updated \(lastUpdated)")
-					.font(.subheadline)
-				//				Spacer()
-				//				if let location = locationManager.location {
-				//					Text("Your location: \(location.latitude), \(location.longitude)")
-				//				}
-			}.padding()
-			List {
-				ForEach($stations) { station in
-					StationView(station: station)
-				}.listStyle(InsetGroupedListStyle())
-			}.refreshable {
+		ZStack {
+			VStack(alignment: .leading) {
+				HStack {
+					Spacer()
+					Text("Updated \(lastUpdated)")
+						.font(.subheadline)
+					//				Spacer()
+					//				if let location = locationManager.location {
+					//					Text("Your location: \(location.latitude), \(location.longitude)")
+					//				}
+				}.padding()
+				List {
+					ForEach($stations) { station in
+						StationView(station: station)
+					}.listStyle(InsetGroupedListStyle())
+				}.refreshable {
+					fetchData()
+				}
+			}.onAppear {
+				locationManager.requestAuthorisation(always: false)
 				fetchData()
-			}
-		}.onAppear {
-			locationManager.requestAuthorisation(always: false)
-			fetchData()
-		} //.ignoresSafeArea(.all, edges: .top)
+			} //.ignoresSafeArea(.all, edges: .top)
+			SplashScreen()
+				.opacity(splashOpacity)
+		}
 	}
 	
 	func fetchData() -> Void {
 		API().fetchStations { response in
 			self.lastUpdated = response.shortDate
 			self.stations = response.stations
+			
+			DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+				withAnimation(.easeIn(duration: 0.3)) {
+						 self.splashOpacity = 0
+				 }
+			}
 		}
 	}
 	
