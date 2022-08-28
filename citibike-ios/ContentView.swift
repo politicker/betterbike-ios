822285273d922187a0545634253783d2823954f5
Harrison Borges <hborges005@gmail.com>
2022-08-27 20:21:30 -0400

    Initial Commit

---
+++ citibike-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  citibike-ios
+//
+//  Created by Harrison Borges on 8/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ citibike-ios/citibike_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  citibike_iosApp.swift
+//  citibike-ios
+//
+//  Created by Harrison Borges on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct citibike_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
