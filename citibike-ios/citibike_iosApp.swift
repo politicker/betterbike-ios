5726e2a185d0aea6679fea83645d62fb4fb0816b
Harrison Borges <hborges005@gmail.com>
2022-09-17 19:02:00 -0400

    add more descriptive scene

--- citibike-ios/citibike_iosApp.swift
+++ citibike-ios/citibike_iosApp.swift
@@ -5,13 +5,13 @@
 //  Created by Harrison Borges on 8/27/22.
 //
 
-import SwiftUI
-
-@main
-struct citibike_iosApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
-}
+//import SwiftUI
+//
+//@main
+//struct citibike_iosApp: App {
+//	var body: some Scene {
+//		WindowGroup {
+//			ContentView()
+//		}
+//	}
+//}
